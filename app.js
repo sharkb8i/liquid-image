0fb4e02effbe48e1ebd67e5203a0597b5c2f3751
Bruno <37672571+sharkb8i@users.noreply.github.com>
2020-05-03 22:38:25 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.2,
+    image1: './woman1.jpg',
+    image2: './woman2.jpg',
+    /* angle: Math.PI / 8,*/
+    displacementImage: './liquid.png'
+});
